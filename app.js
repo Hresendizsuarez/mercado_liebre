0b4d8a5371afd6604f8c00f4403bb31a836dee01
Hresendizsuarez <hresendizsuarez@gmail.com>
2022-01-31 18:48:30 -0600

    Heroku

--- app.js
+++ app.js
@@ -18,9 +18,9 @@ app.get('/login', (req,res) =>{
 
 app.use(express.static(path.join(__dirname, './public')));
 
-app.listen(3006,() =>{
-    console.log('servidor corriendo')
-});
+app.listen(process.env.PORT || 3000, function() {
+    console.log("Servidor corriendo");
+})
     
 
 
